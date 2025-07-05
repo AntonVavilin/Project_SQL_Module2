a815428e5b74b610a90ca5deea877fd64225d2c0
twój login do GitHuba <avavilin@visa.co>
2025-07-05 15:19:06 +0200

    com

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,9 +1,13 @@
 package pl.coderslab;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Welcome to Java!");
+
     }
 }
